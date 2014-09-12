d78582828e7cff5d0adfe9f1d7b81ee8c50d6e8e
Jethro Yu <jethro.yu@hopebaytech.com>
2014-09-12 12:13:09 +0800

    Update nginx version at build

--- build-nginx/build.sh
+++ build-nginx/build.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+PAGESPEED_VER=`curl -s "https://github.com/pagespeed/ngx_pagespeed/releases/" \
+	| grep /tag/ | sed -e 's#.*/v\(.*\)".*#\1#' | head -1 | sed -e 's/-beta//'`
+
+echo ${PAGESPEED_VER}
+sed -i -e "/^ENV PAGESPEED_VER/s/.*/ENV PAGESPEED_VER ${PAGESPEED_VER}/" Dockerfile
+
+rm -rf deb
 docker build -t nginx_pagespeed_builder .
 docker run --cidfile="cid" nginx_pagespeed_builder
 docker cp `cat cid`:/build .
