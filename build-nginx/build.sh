81efe853785bfb2e583e9a6214fc93d8da941264
Jethro Yu <jethro.yu@hopebaytech.com>
2014-09-12 17:11:28 +0800

    use ENV to trace nginx version & auto update when build

--- build-nginx/build.sh
+++ build-nginx/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-PAGESPEED_VER=`curl -s "https://github.com/pagespeed/ngx_pagespeed/releases/" \
+
+NGINX_VERSION=`xidel https://launchpad.net/~nginx/+archive/ubuntu/stable?field.series_filter=trusty  --extract  "//table[@id='packages_list']//td[2]"`
+PAGESPEED_VERSION=`curl -s "https://github.com/pagespeed/ngx_pagespeed/releases/" \
 	| grep /tag/ | sed -e 's#.*/v\(.*\)".*#\1#' | head -1 | sed -e 's/-beta//'`
 
-echo ${PAGESPEED_VER}
-sed -i -e "/^ENV PAGESPEED_VER/s/.*/ENV PAGESPEED_VER ${PAGESPEED_VER}/" Dockerfile
+sed -i -e "/^ENV NGINX_VERSION/s/.*/ENV NGINX_VERSION ${NGINX_VERSION}/" \
+	-e "/^ENV NPS_VERSION/s/.*/ENV NPS_VERSION ${PAGESPEED_VERSION}/" \
+	Dockerfile
+
+echo Nginx: ${NGINX_VERSION}
+echo PageSpeed: ${PAGESPEED_VERSION}
 
 rm -rf deb
 docker build -t nginx_pagespeed_builder .
